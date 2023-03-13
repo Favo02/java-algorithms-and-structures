2435a5404911dbe07d4cc3c9ada072b375ae65a9
Luca Favini <luca.favini.02@gmail.com>
2023-03-13 15:27:47 +0100

    `Vertex` made mutable, removed `MutableVertex`

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,40 +1,60 @@
 public class Vertex<T> {
+
     protected boolean locked;
     protected int x, y;
     protected T value;
     protected String tag;
 
-    public Vertex(int x, int y, T valore, String etichetta) {
+    public Vertex(int x, int y, T valore, String tag) {
         locked = false;
         this.x = x;
         this.y = y;
         this.value = valore;
-        this.tag = etichetta;
-    }
-
-    public void lock() {
-        locked = true;
+        this.tag = tag;
     }
 
     public boolean isLocked() {
         return locked;
     }
 
+    public void lock() {
+        locked = true;
+    }
+
+    public void unlock() {
+        locked = false;
+    }
+
     public int getX() {
         return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
     }
 
     public int getY() {
         return y;
     }
 
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public T getValue() {
         return value;
     }
 
-    public String getTag() {
-        return tag;
+    public void setValue(T value) {
+        this.value = value;
     }
 
-    
+    public String getTag() {
+        return tag;
+    }    
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
 }
