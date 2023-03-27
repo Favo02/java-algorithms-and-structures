f1d7fba8ee068ee61c363aab2ec077d6d34cf7aa
Luca Favini <luca.favini.02@gmail.com>
2023-03-27 13:59:53 +0200

    added `Edge<TKey>`

---
+++ src/Structures/Graphs/Edge.java
@@ -0,0 +1,62 @@
+package Structures.Graphs;
+
+import java.util.Objects;
+
+public class Edge<TKey> {
+  
+  private Vertex<TKey> vertexFrom;
+  private Vertex<TKey> vertexTo;
+
+  public Edge(Vertex<TKey> vertexFrom, Vertex<TKey> vertexTo) {
+    this.vertexFrom = vertexFrom;
+    this.vertexTo = vertexTo;
+  }
+
+  public Vertex<TKey> getVertexFrom() {
+    return vertexFrom;
+  }
+
+  public Vertex<TKey> getVertexTo() {
+    return vertexTo;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(vertexFrom, vertexTo);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    @SuppressWarnings("unchecked")
+    Edge<TKey> other = (Edge<TKey>) obj;
+    if (vertexFrom == null) {
+      if (other.vertexFrom != null)
+        return false;
+    } else if (!vertexFrom.equals(other.vertexFrom))
+      return false;
+    if (vertexTo == null) {
+      if (other.vertexTo != null)
+        return false;
+    } else if (!vertexTo.equals(other.vertexTo))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Edge[vertexFrom=");
+    sb.append(vertexFrom);
+    sb.append(",vertexTo=");
+    sb.append(vertexTo);
+    sb.append("]");
+    return sb.toString();
+  }
+
+}
