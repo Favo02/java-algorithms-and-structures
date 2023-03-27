2c2dc71a55b938fba991451d8846ffc4df212a34
Luca Favini <luca.favini.02@gmail.com>
2023-03-27 13:48:29 +0200

    added `Vertex<TKey>`

---
+++ src/Structures/Graphs/Vertex.java
@@ -0,0 +1,48 @@
+package Structures.Graphs;
+
+import java.util.Objects;
+
+public class Vertex<TKey> {
+
+  private TKey key;
+
+  public Vertex(TKey key) {
+    this.key = key;
+  }
+
+  public TKey getKey() {
+    return key;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(key);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Vertex other = (Vertex) obj;
+    if (key == null) {
+      if (other.key != null)
+        return false;
+    } else if (!key.equals(other.key))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Vertex[key=");
+    sb.append(key);
+    sb.append("]");
+    return sb.toString();
+  }
+
+}
