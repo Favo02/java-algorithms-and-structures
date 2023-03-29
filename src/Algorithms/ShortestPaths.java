33daa683381a79a95bdabf4473b1b7de58c3d052
Luca Favini <luca.favini.02@gmail.com>
2023-03-29 14:09:51 +0200

    optimized `dijkstra`: use `FibonacciHeap`

--- src/Algorithms/ShortestPaths.java
+++ src/Algorithms/ShortestPaths.java
@@ -1,11 +1,10 @@
 package Algorithms;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
+import Structures.FibonacciHeap;
 import Structures.Graphs.Vertex;
 import Structures.Graphs.WeightedEdge;
 import Structures.Graphs.Implementations.Weighted.IncidenceListGraph;
@@ -16,33 +15,27 @@ public class ShortestPaths {
     
     Map<Vertex<TKey>, Long> distances = new HashMap<>();
 
-    Set<Vertex<TKey>> unvisited = new HashSet<>(); // TODO use minheap
+    FibonacciHeap<Vertex<TKey>> queueHeap = new FibonacciHeap<>();
 
     Iterator<Vertex<TKey>> vertexesIterator = graph.getVertexesIterator();
     while (vertexesIterator.hasNext()) {
-      unvisited.add(vertexesIterator.next());
+      queueHeap.enqueue(vertexesIterator.next(), Long.MAX_VALUE);
     }
 
     distances.put(start, Long.valueOf(0));
 
-    while (unvisited.size() != 0) {
+    while (queueHeap.size() != 0) {
 
-      // TODO poll from minheap
-      Vertex<TKey> closest = null;
-      for (Vertex<TKey> vertex : unvisited) {
-        if (closest == null || distances.get(vertex) < distances.get(closest)) {
-          closest = vertex;
-        }
-      }
-
-      unvisited.remove(closest);
+      FibonacciHeap.Entry<Vertex<TKey>> entry = queueHeap.dequeueMin();
+      Vertex<TKey> closest = entry.getValue();
+      long closestDistance = entry.getPriority();
 
       Iterator<WeightedEdge<TKey>> incidentIterator = graph.getAdjacentByVertexIterator(closest);
       while (incidentIterator.hasNext()) {
         WeightedEdge<TKey> incident = incidentIterator.next();
 
         long oldDistance = distances.get(incident.getVertexTo());
-        long newDistance = distances.get(closest) + incident.getWeight();
+        long newDistance = closestDistance + incident.getWeight();
         
         if (newDistance < oldDistance) {
           distances.put(incident.getVertexTo(), newDistance);
